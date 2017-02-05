e4521bf84695542f9dcaf344361ad54395db0cfd
Drew DeVault <sir@cmpwn.com>
2017-02-05 17:48:27 -0500

    Move error handling to shared code

--- git/app.py
+++ git/app.py
@@ -22,21 +22,3 @@ except:
 from git.blueprints.cgit import cgit
 
 app.register_blueprint(cgit)
-
-if not app.debug:
-    @app.errorhandler(500)
-    def handle_500(e):
-        # shit
-        try:
-            db.rollback()
-            db.close()
-        except:
-            # shit shit
-            sys.exit(1)
-        return render_template("internal_error.html"), 500
-
-@app.errorhandler(404)
-def handle_404(e):
-    if request.path.startswith("/api"):
-        return { "errors": [ { "reason": "404 not found" } ] }, 404
-    return render_template("not_found.html"), 404
