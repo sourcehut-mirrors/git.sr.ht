3e413e73bb278699f90b63310ab0cb879c494b32
Drew DeVault <sir@cmpwn.com>
2018-09-19 23:10:20 -0400

    Lay out summary page

--- gitsrht/blueprints/repo.py
+++ gitsrht/blueprints/repo.py
@@ -1,11 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, abort
 from flask_login import current_user
+from gitsrht.access import get_repo, has_access, UserAccess
 from gitsrht.types import User, Repository
 
 repo = Blueprint('repo', __name__)
 
 # TODO: !! SECURITY !!
 
-@repo.route("/<owner>/<name>")
-def summary(owner, name):
-    return "hi!"
+@repo.route("/<owner>/<repo>")
+def summary(owner, repo):
+    owner, repo = get_repo(owner, repo)
+    if not has_access(repo, UserAccess.read):
+        abort(401)
+    with open("/home/sircmpwn/sources/wlroots/README.md") as f:
+        rm = f.read()
+    return render_template("summary.html", owner=owner, repo=repo, rm=rm)
