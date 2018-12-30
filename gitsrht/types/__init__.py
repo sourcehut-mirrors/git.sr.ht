6ca9fde9de2eeabdc1f48f01bc5147cab1f62b33
Drew DeVault <sir@cmpwn.com>
2018-12-30 15:16:51 -0500

    Initialize mixins in __init__

--- gitsrht/types/__init__.py
+++ gitsrht/types/__init__.py
@@ -2,10 +2,12 @@ from srht.database import Base
 from srht.oauth import ExternalUserMixin, ExternalOAuthTokenMixin
 
 class User(Base, ExternalUserMixin):
-    pass
+    def __init__(*args, **kwargs):
+        ExternalUserMixin.__init__(*args, **kwargs)
 
 class OAuthToken(Base, ExternalOAuthTokenMixin):
-    pass
+    def __init__(*args, **kwargs):
+        ExternalOAuthTokenMixin.__init__(*args, **kwargs)
 
 from .repository import Repository, RepoVisibility
 from .webhook import Webhook
