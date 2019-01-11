e1f9059d7c2e61b3ff758edffdd79ba1dcd72a2c
Ludovic Chabant <ludovic@chabant.com>
2019-01-11 06:55:48 +0000

    Use new way of running debug/local services

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from gitsrht.app import app
-from srht.config import cfg, cfgi
-
-import os
-
-app.static_folder = os.path.join(os.getcwd(), "static")
-
 if __name__ == '__main__':
-    app.run(host=cfg("git.sr.ht", "debug-host"),
-            port=cfgi("git.sr.ht", "debug-port"),
-            debug=True)
+    from srht.debug import run_service
+    from gitsrht.app import app
+    run_service(app)
