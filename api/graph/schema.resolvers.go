sourcehut-mirrors/git.sr.ht

commit 8a769ecedb1f0748e82d792622b07219a5ac921d

Prohibit repos named . or ..

api/graph/schema.resolvers.go
@@ -72,6 +72,9 @@ func (r *mutationResolver) CreateRepository(ctx context.Context, name string, vi
 		return nil, fmt.Errorf("Invalid repository name '%s' (must match %s)",
 			name, repoNameRE.String())
 	}
+	if name == "." || name == ".." {
+		return nil, fmt.Errorf("Invalid repository name '%s' (must not be . or ..)", name))
+	}
 
 	conf := config.ForContext(ctx)
 	repoStore, ok := conf.Get("git.sr.ht", "repos")

commit 91333aaf1cf03588cd3d4dfcf715623b014a69de

fix typo in sources for the api binary which broke the build
Regression from commit 8a769ecedb1f0748e82d792622b07219a5ac921d.

api/graph/schema.resolvers.go
@@ -73,7 +73,7 @@ func (r *mutationResolver) CreateRepository(ctx context.Context, name string, vi
 			name, repoNameRE.String())
 	}
 	if name == "." || name == ".." {
-		return nil, fmt.Errorf("Invalid repository name '%s' (must not be . or ..)", name))
+		return nil, fmt.Errorf("Invalid repository name '%s' (must not be . or ..)", name)
 	}
 
 	conf := config.ForContext(ctx)