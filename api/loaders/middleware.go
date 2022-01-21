d4b779bac90321e42b4604d72b75019f6b391b75
Adnan Maolood <me@adnano.co>
2022-01-20 19:02:53 -0500

    api: Remove generated files, update Makefile
    References: https://todo.sr.ht/~sircmpwn/sr.ht/307

--- api/loaders/middleware.go
+++ api/loaders/middleware.go
@@ -1,11 +1,5 @@
 package loaders
 
-//go:generate ./gen RepositoriesByIDLoader int api/graph/model.Repository
-//go:generate ./gen RepositoriesByNameLoader string api/graph/model.Repository
-//go:generate ./gen RepositoriesByOwnerRepoNameLoader [2]string api/graph/model.Repository
-//go:generate ./gen UsersByIDLoader int api/graph/model.User
-//go:generate ./gen UsersByNameLoader string api/graph/model.User
-
 import (
 	"context"
 	"database/sql"
