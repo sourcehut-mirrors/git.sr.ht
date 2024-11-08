39720caa69ec0b2ed4cd2fcf662ef383f954fc52
Conrad Hoffmann <ch@bitfehler.net>
2024-11-08 15:44:36 +0100

    Reduce setup.py to a stub
    All configuration is now in pyproject.toml, all tooling set up to use
    it. The stub remains (for now) to execute the call to `make`, as such
    calls are not supported by pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+from setuptools import setup
 import subprocess
 import os
 import sys
@@ -13,39 +13,4 @@ subp = subprocess.run([make, "SRHT_PATH=" + srht_path])
 if subp.returncode != 0:
     sys.exit(subp.returncode)
 
-ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'],
-      stdout=subprocess.PIPE).stdout.decode().strip()
-
-setup(
-  name = 'gitsrht',
-  packages = [
-      'gitsrht',
-      'gitsrht.types',
-      'gitsrht.blueprints',
-      'gitsrht.blueprints.api',
-      'gitsrht.alembic',
-      'gitsrht.alembic.versions'
-  ],
-  version = ver,
-  description = 'git.sr.ht website',
-  author = 'Drew DeVault',
-  author_email = 'sir@cmpwn.com',
-  url = 'https://git.sr.ht/~sircmpwn/git.sr.ht',
-  install_requires = ['srht', 'scmsrht', 'pygit2', 'minio'],
-  license = 'AGPL-3.0',
-  package_data={
-      'gitsrht': [
-          'templates/*.html',
-          'static/*',
-          'static/icons/*',
-          'hooks/*',
-          'schema.graphqls',
-          'default_query.graphql',
-      ]
-  },
-  scripts = [
-      'gitsrht-initdb',
-      'gitsrht-migrate',
-      'gitsrht-periodic',
-  ]
-)
+setup()
