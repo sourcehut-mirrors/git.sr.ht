sourcehut-mirrors/git.sr.ht

commit efd03947f91a4c453a4a12086d2e21222f345178

API, update-hook: deliver pre-receive/post-receive hooks

api/graph/model/githook.go
@@ -50,18 +50,21 @@ func NewUpdatedRef(
 }
 
 func (ref *UpdatedRef) Ref() (*Reference, error) {
+	if ref.ref == nil {
+		return nil, nil
+	}
 	return &Reference{ref.repo, ref.ref}, nil
 }
 
 func (ref *UpdatedRef) Old() (Object, error) {
-	if ref.oldHash == nil {
+	if ref.oldHash == nil || ref.oldHash.IsZero() {
 		return nil, nil
 	}
 	return LookupObject(ref.repo.Repo(), *ref.oldHash)
 }
 
 func (ref *UpdatedRef) New() (Object, error) {
-	if ref.newHash == nil {
+	if ref.newHash == nil || ref.oldHash.IsZero() {
 		return nil, nil
 	}
 	return LookupObject(ref.repo.Repo(), *ref.newHash)

api/graph/schema.graphqls
@@ -254,7 +254,8 @@ type RepositoryEvent implements WebhookPayload {
 }
 
 type UpdatedRef {
-  ref: Reference!
+  # Is null when pushing a new reference
+  ref: Reference
   # Is null when pushing a new reference
   old: Object
   # Is null when deleting a reference
@@ -604,6 +605,7 @@ input UpdatedRefInput {
 
 input GitEventInput {
   repositoryID: Int!
+  event: WebhookEvent!
   updates: [UpdatedRefInput]!
 }
 

api/graph/schema.resolvers.go
@@ -1084,7 +1084,7 @@ func (r *mutationResolver) DeleteUser(ctx context.Context) (int, error) {
 
 // DeliverGitHook is the resolver for the deliverGitHook field.
 func (r *mutationResolver) DeliverGitHook(ctx context.Context, input model.GitEventInput) (bool, error) {
-	panic(fmt.Errorf("not implemented: DeliverGitHook - deliverGitHook"))
+	return webhooks.DeliverGitEvent(ctx, input), nil
 }
 
 // Version is the resolver for the version field.

api/webhooks/webhooks.go
@@ -6,10 +6,12 @@ import (
 
 	"git.sr.ht/~sircmpwn/core-go/auth"
 	"git.sr.ht/~sircmpwn/core-go/webhooks"
-	sq "github.com/Masterminds/squirrel"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/google/uuid"
+	sq "github.com/Masterminds/squirrel"
 
 	"git.sr.ht/~sircmpwn/git.sr.ht/api/graph/model"
+	"git.sr.ht/~sircmpwn/git.sr.ht/api/loaders"
 )
 
 func deliverUserWebhook(ctx context.Context, event model.WebhookEvent,
@@ -34,4 +36,65 @@ func DeliverRepoEvent(ctx context.Context,
 		Repository: repository,
 	}
 	deliverUserWebhook(ctx, event, &payload, payloadUUID)
+}
+
+func DeliverGitEvent(ctx context.Context, input model.GitEventInput) bool {
+	payloadUUID := uuid.New()
+
+	user := auth.ForContext(ctx)
+	pusher, err := loaders.ForContext(ctx).UsersByID.Load(user.UserID)
+	if err != nil {
+		panic(err)
+	}
+
+	repo, err := loaders.ForContext(ctx).RepositoriesByID.Load(input.RepositoryID)
+	if err != nil {
+		panic(err)
+	}
+
+	grepo := repo.Repo()
+
+	var updates []*model.UpdatedRef
+	for _, ref := range input.Updates {
+		var (
+			gref *plumbing.Reference
+			err  error
+		)
+		oldHash := plumbing.NewHash(ref.Old)
+		newHash := plumbing.NewHash(ref.New)
+
+		if !oldHash.IsZero() {
+			grepo.Lock()
+			gref, err = grepo.Reference(
+				plumbing.ReferenceName(ref.Ref), true)
+			grepo.Unlock()
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		upd := model.NewUpdatedRef(repo, gref,
+			&oldHash, &newHash)
+		updates = append(updates, &upd)
+	}
+
+	payload := model.GitEvent{
+		UUID:       payloadUUID.String(),
+		Event:      input.Event,
+		Date:       time.Now().UTC(),
+		Repository: repo,
+		Pusher:     pusher,
+		Updates:    updates,
+	}
+
+	q := webhooks.ForContext(ctx)
+	query := sq.
+		Select().
+		From(`gql_git_wh_sub sub`).
+		Where(`sub.repo_id = ?`, repo.ID)
+	q.Schedule(ctx, query, "git", input.Event.String(),
+		payloadUUID, payload)
+
+	// TODO: Synchronous delivery
+	return true
 }

gitsrht-update-hook/main.go
@@ -19,16 +19,18 @@ var (
 func main() {
 	log.SetFlags(0)
 	logger.Printf("%v", os.Args)
-	// The update hook is run on the update and post-update git hooks, and also
-	// runs a third stage directly. The first two stages are performance
-	// critical and take place while the user is blocked at their terminal. The
-	// third stage is done in the background.
+	// The update hook is run on the update and post-update git hooks, and
+	// also runs a third stage directly. The first two stages are
+	// performance critical and take place while the user is blocked at
+	// their terminal. The third stage is done in the background.
 	if os.Args[0] == "hooks/pre-receive" {
-		preReceive()
+		receiveHook("GIT_PRE_RECEIVE")
 	} else if os.Args[0] == "hooks/update" {
 		update()
 	} else if os.Args[0] == "hooks/post-update" {
 		postUpdate()
+	} else if os.Args[0] == "hooks/post-receive" {
+		receiveHook("GIT_POST_RECEIVE")
 	} else if os.Args[0] == "hooks/stage-3" {
 		stage3()
 	} else {

gitsrht-update-hook/post-update.go
@@ -189,6 +189,8 @@ func postUpdate() {
 	}
 
 	loadOptions()
+
+	// Legacy webhook payload
 	payload := WebhookPayload{
 		Push:     pushUuid,
 		PushOpts: options,

gitsrht-update-hook/pre-receive.go
@@ -1,21 +1,0 @@
-package main
-
-import (
-	"log"
-	"os"
-)
-
-func preReceive() {
-	// TODO: This would be a good place to enforce branch update restrictions
-	// and such, or to check OWNERS, etc.
-	pushUuid, ok := os.LookupEnv("SRHT_PUSH")
-	if !ok {
-		logger.Fatal("Missing SRHT_PUSH in environment, configuration error?")
-	}
-	logger.Printf("Running pre-receive for push %s", pushUuid)
-
-	loadOptions()
-	if _, ok := options["debug"]; ok {
-		log.Printf("debug: %s", pushUuid)
-	}
-}

gitsrht-update-hook/receive.go
@@ -1,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"git.sr.ht/~sircmpwn/core-go/client"
+	coreconfig "git.sr.ht/~sircmpwn/core-go/config"
+)
+
+func receiveHook(event string) {
+	// TODO: We should consider moving our internal post-update logic to
+	// post-receive instead. We can ditch the update hook as well if we go
+	// this route, and drop the Redis usage.
+	pushUuid, ok := os.LookupEnv("SRHT_PUSH")
+	if !ok {
+		logger.Fatal("Missing SRHT_PUSH in environment, configuration error?")
+	}
+	logger.Printf("Running %s for push %s", event, pushUuid)
+
+	var pcontext PushContext
+	contextJson, ctxOk := os.LookupEnv("SRHT_PUSH_CTX")
+	if !ctxOk {
+		logger.Fatal("Missing required variables in environment, " +
+			"configuration error?")
+	}
+	if err := json.Unmarshal([]byte(contextJson), &pcontext); err != nil {
+		logger.Fatalf("unmarshal SRHT_PUSH_CTX: %v", err)
+	}
+
+	loadOptions()
+	if _, ok := options["debug"]; ok {
+		log.Printf("debug: %s", pushUuid)
+	}
+
+	var updates []UpdatedRefInput
+
+	rd := bufio.NewReader(os.Stdin)
+	for {
+		line, prefix, err := rd.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if prefix {
+			// Drop ref if it exceeds bufio buffer length
+			break
+		}
+		items := strings.SplitN(string(line), " ", 3)
+		if len(items) != 3 {
+			panic("git invariant broken")
+		}
+		oldOid := items[0]
+		newOid := items[1]
+		refName := items[2]
+		updates = append(updates, UpdatedRefInput{
+			Ref: refName,
+			Old: oldOid,
+			New: newOid,
+		})
+	}
+
+	ctx := coreconfig.Context(context.Background(), config, "git.sr.ht")
+	err := client.Do(ctx, pcontext.User.Name, "git.sr.ht", client.GraphQLQuery{
+		Query: `
+		mutation SubmitGitHook($input: GitEventInput!) {
+			deliverGitHook(input: $input)
+		}`,
+		Variables: map[string]interface{}{
+			"input": &GitEventInput{
+				RepositoryID: pcontext.Repo.Id,
+				Event:        event,
+				Updates:      updates,
+			},
+		},
+	}, struct{}{})
+
+	if err != nil {
+		logger.Fatalf("Failed to execute pre-receive hooks: %s",
+		err.Error())
+	}
+}

commit b2ed1936b02dfe5a71d004845492c9e88448806d

githook: look at newHash, not oldHash
Since hub.sr.ht's 1678dec5 commit, the very first push to a git
repository linked to a project is not reflected anymore in hub, because
the payload of its associated webhook call to hub has no "new" attribute
and the update is ignored in hubsrht/blueprints/webhooks.py#L105.

We did not see the issue before because hub did not rely on "new" to
generate its associated event.

Fixes: efd03947 ("API, update-hook: deliver pre-receive/post-receive hooks")
Signed-off-by: Simon Martin <simon@nasilyan.com>

api/graph/model/githook.go
@@ -64,7 +64,7 @@ func (ref *UpdatedRef) Old() (Object, error) {
 }
 
 func (ref *UpdatedRef) New() (Object, error) {
-	if ref.newHash == nil || ref.oldHash.IsZero() {
+	if ref.newHash == nil || ref.newHash.IsZero() {
 		return nil, nil
 	}
 	return LookupObject(ref.repo.Repo(), *ref.newHash)