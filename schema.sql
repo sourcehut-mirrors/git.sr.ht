sourcehut-mirrors/git.sr.ht

commit 87d20829cacc328c2b920e60131fe725593240b9

Add `sshkey` table for deploy keys

schema.sql
@@ -129,6 +129,23 @@ CREATE TABLE redirect (
 	new_repo_id integer NOT NULL REFERENCES repository(id) ON DELETE CASCADE
 );
 
+CREATE TABLE sshkey (
+	id serial PRIMARY KEY,
+	rid uuid NOT NULL UNIQUE DEFAULT gen_uuidv7(),
+	created timestamp without time zone,
+	repo_id integer REFERENCES repository(id) ON DELETE CASCADE,
+	key character varying(4096) NOT NULL,
+	key_type character varying(256) NOT NULL,
+	fingerprint_sha256 character varying(512) NOT NULL,
+	comment character varying(256),
+	access access_mode NOT NULL,
+	last_used timestamp without time zone,
+);
+
+ALTER TABLE sshkey
+	ADD CONSTRAINT sshkey_repo_id_fingerprint_sha256_key
+	UNIQUE (repo_id, fingerprint_sha256);
+
 -- GraphQL webhooks
 CREATE TABLE gql_user_wh_sub (
 	id serial PRIMARY KEY,

migrations/0007_deploy_keys.sql
@@ -1,0 +1,20 @@
+-- +brant Up
+CREATE TABLE sshkey (
+	id serial PRIMARY KEY,
+	rid uuid NOT NULL UNIQUE DEFAULT gen_uuidv7(),
+	created timestamp without time zone,
+	repo_id integer REFERENCES repository(id) ON DELETE CASCADE,
+	key character varying(4096) NOT NULL,
+	key_type character varying(256) NOT NULL,
+	fingerprint_sha256 character varying(512) NOT NULL,
+	comment character varying(256),
+	access access_mode NOT NULL,
+	last_used timestamp without time zone
+);
+
+ALTER TABLE sshkey
+	ADD CONSTRAINT sshkey_repo_id_fingerprint_sha256_key
+	UNIQUE (repo_id, fingerprint_sha256);
+
+-- +brant Down
+DROP TABLE sshkey;

commit 68c7201688ba928d46fb781a28b6ca715f4e421d

schema.sql: remove trailing comma
I started to get failures in sr.ht-integration-testing because the
database initialization fails because the trailing comma, that is
reported as a syntax error.

It's happening intermittently so this is weird, but the comma is not in
the migration script and we don't have any in any of the other CREATE
TABLE statements (and AFAIK it's not allowed), so let's remove it.

Fixes: 87d20829 ("Add `sshkey` table for deploy keys")
Signed-off-by: Simon Martin <simon@nasilyan.com>

schema.sql
@@ -139,7 +139,7 @@ CREATE TABLE sshkey (
 	fingerprint_sha256 character varying(512) NOT NULL,
 	comment character varying(256),
 	access access_mode NOT NULL,
-	last_used timestamp without time zone,
+	last_used timestamp without time zone
 );
 
 ALTER TABLE sshkey